ee24ec33102be2e6227a7e47164ce66a945c23ba
Rebecca <techybecky21@gmail.com>
2023-05-16 00:15:41 +0100

    10x10 grid fits in container

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const container = document.querySelector('.container');
 const newSketch = document.querySelector('.new')
 const reset = document.querySelector('.reset')
 
-
-
-function resetGrid(rows,cols){
+function createGrid(rows,cols){
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     for(let i = 0; i< rows * cols; i++){
@@ -16,26 +14,4 @@ function resetGrid(rows,cols){
             })
     }
 }
-
-reset.addEventListener('click', ()=>{
-   resetGrid(10,10)
-    
-})
-
-newSketch.addEventListener('click', ()=>{
-   
-    let userInput = prompt('Choose number of squares per side, i.e. 64 for 64x64 grid')
-    container.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
-
-    for(let i = 0; i< userInput * userInput; i++){
-        let grid = document.createElement('div');
-            grid.classList.add('grid')  
-            container.appendChild(grid);
-            grid.addEventListener('mouseover',()=>{
-                grid.style.background = 'hsla(97, 100%, 78%, 1)'
-            })
-    }
-    
-})
-
+createGrid(10,10)
