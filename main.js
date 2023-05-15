898b41bed8cc1a2eeb47305f526b7b74fa9287bc
Rebecca <techybecky21@gmail.com>
2023-05-16 00:00:09 +0100

    reset button works 10x10

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const container = document.querySelector('.container');
+const newSketch = document.querySelector('.new')
+const reset = document.querySelector('.reset')
 
-function makeGrid(rows,cols){
-    for(let i = 0; i< (rows * cols); i++){
-        const grid = document.createElement('div');
+
+
+function resetGrid(rows,cols){
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    for(let i = 0; i< rows * cols; i++){
+        let grid = document.createElement('div');
+            grid.classList.add('grid')  
+            container.appendChild(grid);
+            grid.addEventListener('mouseover',()=>{
+                grid.style.background = 'hsla(97, 100%, 78%, 1)'
+            })
+    }
+}
+
+reset.addEventListener('click', ()=>{
+   resetGrid(10,10)
+    
+})
+
+newSketch.addEventListener('click', ()=>{
+   
+    let userInput = prompt('Choose number of squares per side, i.e. 64 for 64x64 grid')
+    container.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+
+    for(let i = 0; i< userInput * userInput; i++){
+        let grid = document.createElement('div');
             grid.classList.add('grid')  
             container.appendChild(grid);
             grid.addEventListener('mouseover',()=>{
@@ -10,10 +37,5 @@ function makeGrid(rows,cols){
             })
     }
     
-}
-
-makeGrid(16,16)
-
-
-
+})
 
