9c8c425f083d71d9ae97de5da1633285a307a57a
Rebecca <techybecky21@gmail.com>
2023-05-16 21:13:43 +0100

    update styles.css

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ function createGrid(rows,cols){
     container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     for(let i = 0; i< rows * cols; i++){
-        let grid = document.createElement('div');
-            grid.classList.add('grid')  
+        let grid = document.createElement('div'); 
             container.appendChild(grid);
+            grid.style.border = '1px solid gray'
             grid.addEventListener('mouseover',()=>{
                 grid.style.background = 'hsla(97, 100%, 78%, 1)'
             })
     }
 }
-createGrid(10,10)
+createGrid(64,64)
